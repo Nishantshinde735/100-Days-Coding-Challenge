6ae19ee2f924d9eb215a6b50631237ecc072da32
Nishantshinde735 <162629124+Nishantshinde735@users.noreply.github.com>
2024-03-09 23:45:27 +0530

    Create Basic_Datatypes.cpp

---
+++ March/Day_1/Basic_Datatypes.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a = 123;
+
+    cout << a << endl;
+   
+    char s ='n';
+
+    cout << s << endl;
+
+    float f = 1.2;
+
+    cout << f << endl;
+
+    double dl=1.234;
+
+    cout << dl << endl;
+
+    int size= sizeof(f);
+    cout<< "size of dl is:" << size<<endl;
+    
+}
