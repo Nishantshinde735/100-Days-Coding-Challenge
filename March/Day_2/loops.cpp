5223acc061ebe19445a9ac1f5dd01b51c13d42e3
Nishantshinde735 <162629124+Nishantshinde735@users.noreply.github.com>
2024-03-10 21:08:57 +0530

    Add files via upload

---
+++ March/Day_2/loops.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+    int a;
+     cout<< "Enter number a :"<<endl ;
+     cin>>a;
+
+   if(a>0)
+     cout<<"a is positive" << endl;
+   else
+    {
+     if(a<0)
+      cout<< " a is negative"<< endl;
+    
+     else
+      cout<< "a is 0"<< endl;
+    }
+
+}
