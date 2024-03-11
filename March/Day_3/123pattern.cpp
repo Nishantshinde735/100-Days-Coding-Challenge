9bf9f88ce0fc5f96879bd18bccf4219a9ffe2dbf
Nishantshinde735 <162629124+Nishantshinde735@users.noreply.github.com>
2024-03-11 21:31:35 +0530

    Add files via upload

---
+++ March/Day_3/123pattern.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n;
+  cin>>n ;
+
+  int i=n;
+
+  while(i<=n)
+  {
+    int j=1;
+      while(j<=n)
+       {cout<<j;
+  
+    
+         j=j+1;
+       }
+  
+  
+    i=i+1;
+    cout<<i;
+  }
+  cout<<endl;
+
+}
