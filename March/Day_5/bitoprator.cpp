b6cf7bb0193878304abe59e267965fe4295b8d79
Nishantshinde735 <162629124+Nishantshinde735@users.noreply.github.com>
2024-03-13 22:42:45 +0530

    Add files via upload

---
+++ March/Day_5/bitoprator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int countSetBits(int num) {
+    int count = 0;
+    while (num) {
+        count += num & 1;
+        num >>= 1;
+    }
+    return count;
+}
+
+int main() {
+    int number;
+    std::cout << "Enter an integer: ";
+    std::cin >> number;
+
+    int result = countSetBits(number);
+    std::cout << "Number of set bits: " << result << std::endl;
+
+    return 0;
+}
