0e3b9a35a1ef0177076b9443de2b2f4efc8e0228
Nishantshinde735 <162629124+Nishantshinde735@users.noreply.github.com>
2024-03-25 20:25:58 +0530

    Selection_sort.cpp

---
+++ March/Day_17/Selection_sort.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+void selectionSort(vector<int>& arr) {
+    int n = arr.size();
+
+    for (int i = 0; i < n - 1; ++i) {
+     
+        int minIndex = i;
+        for (int j = i + 1; j < n; ++j) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        if (minIndex != i) {
+            swap(arr[i], arr[minIndex]);
+        }
+    }
+}
+
+int main() {
+    // Example
+    vector<int> arr = {64, 25, 12, 22, 11};
+
+    // Perform selection sort
+    selectionSort(arr);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < arr.size(); ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
