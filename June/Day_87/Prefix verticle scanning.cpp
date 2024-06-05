9dd4bcb997aa56b9cd3429317e59a0c09df1b908
Nishantshinde735 <162629124+Nishantshinde735@users.noreply.github.com>
2024-06-05 09:46:45 +0530

    Create Prefix verticle scanning.cpp

---
+++ June/Day_87/Prefix verticle scanning.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+string longestCommonPrefix(vector<string>& strs) {
+    if (strs.empty()) return "";
+    for (int i = 0; i < strs[0].size(); ++i) {
+        char c = strs[0][i];
+        for (int j = 1; j < strs.size(); ++j) {
+            if (i >= strs[j].size() || strs[j][i] != c) {
+                return strs[0].substr(0, i);
+            }
+        }
+    }
+    return strs[0];
+}
+
+int main() {
+    vector<string> strs = {"flower", "flow", "flight"};
+    cout << "Longest Common Prefix: " << longestCommonPrefix(strs) << endl;
+    return 0;
+}
