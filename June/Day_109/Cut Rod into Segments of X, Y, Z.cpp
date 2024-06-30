81a8dd31a156db8c47614da8a567b57807ad1cd1
Nishantshinde735 <162629124+Nishantshinde735@users.noreply.github.com>
2024-06-30 18:36:32 +0530

    Cut Rod into Segments of X, Y, Z.cpp

---
+++ June/Day_109/Cut Rod into Segments of X, Y, Z.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maximizeCutSegments(int n, int x, int y, int z) {
+        vector<int> dp(n + 1, -1);
+        dp[0] = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            if (i - x >= 0 && dp[i - x] != -1) {
+                dp[i] = max(dp[i], dp[i - x] + 1);
+            }
+            if (i - y >= 0 && dp[i - y] != -1) {
+                dp[i] = max(dp[i], dp[i - y] + 1);
+            }
+            if (i - z >= 0 && dp[i - z] != -1) {
+                dp[i] = max(dp[i], dp[i - z] + 1);
+            }
+        }
+        
+        return dp[n];
+    }
+};
