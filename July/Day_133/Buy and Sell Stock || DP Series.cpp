ce2ec5926417b687e0186c29b44570575fea9d37
Nishantshinde735 <162629124+Nishantshinde735@users.noreply.github.com>
2024-07-26 00:39:39 +0530

    Buy and Sell Stock || DP Series.cpp

---
+++ July/Day_133/Buy and Sell Stock || DP Series.cpp
@@ -0,0 +1,15 @@
+def maxProfit(prices):
+    min_price = float('inf')
+    max_profit = 0
+    
+    for price in prices:
+        if price < min_price:
+            min_price = price
+        elif price - min_price > max_profit:
+            max_profit = price - min_price
+    
+    return max_profit
+
+# Example usage:
+prices = [7, 1, 5, 3, 6, 4]
+print(maxProfit(prices))  # Output: 5
